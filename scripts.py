b73f6d7da854d3fc353798bcf845fac515d5df42
Valery Efremov <valeriy131100@gmail.com>
2021-11-19 19:36:27 +0300

    Add user-friendly interface

--- scripts.py
+++ scripts.py
@@ -35,3 +35,43 @@ def create_commendation(schoolkid, lesson, commendations_list):
         teacher=lesson.teacher,
         created=lesson.date
     )
+
+
+def get_schoolkid():
+    while True:
+        print('Введите имя ученика:')
+        user_input = input()
+        try:
+            schoolkid = models.Schoolkid.objects.get(full_name__contains=user_input)
+        except models.Schoolkid.DoesNotExist:
+            print('Не удалось найти ученика с подобным именем, проверьте свой запрос на опечатки')
+        except models.Schoolkid.MultipleObjectsReturned:
+            print('Запрос вернул более чем одного ученика. Пожалуйста, уточните его')
+        else:
+            return schoolkid
+
+
+def get_random_lesson_by_subject(schoolkid):
+    while True:
+        print('Введите название предмета:')
+        user_input = input()
+        try:
+            subject = models.Subject.objects.get(title=user_input, year_of_study=schoolkid.year_of_study)
+        except models.Lesson.DoesNotExist:
+            print('Не удалось найти предмет с подобным названием на вашем году обучения,'
+                  'проверьте свой запрос на опечатки')
+        else:
+            lessons = models.Lesson.objects.filter(subject=subject, group_letter=schoolkid.group_letter)
+
+            return random.choice(lessons)
+
+
+schoolkid = get_schoolkid()
+lesson = get_random_lesson_by_subject(schoolkid)
+
+fix_marks(schoolkid)
+print(f'Оценки {schoolkid} исправлены')
+remove_chastisements(schoolkid)
+print(f'Замечания {schoolkid} удалены')
+create_commendation(schoolkid, lesson, COMMENDATIONS)
+print(f'Создана похвала по предмету {lesson}')
